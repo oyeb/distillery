d378548c522e8f0800f5ee0beb9cc51fabf01690
Ananya Bahadur <ananya95@gmail.com>
2020-06-01 20:22:01 +0530

    ci: Update ci

--- tests/dummy_spl.hh
+++ tests/dummy_spl.hh
@@ -1,6 +1,6 @@
 #ifndef DISTILLERY_TEST_DUMMYGRAPH_SPECIALIZATION_H
 #define DISTILLERY_TEST_DUMMYGRAPH_SPECIALIZATION_H
-#include "dummy.h"
+#include "dummy.hh"
 #include <graph_abstractions/cfg_interface.hh>
 
 struct DummyGraphSpecialization
