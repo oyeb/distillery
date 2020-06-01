2237ed3be9630cfde78fe26d7908a4bc378ecf87
Ananya Bahadur <ananya95@gmail.com>
2020-06-01 19:53:03 +0530

    feat: Adds Program analysis interfaces

--- include/program_analysis/data_flow_object.hh
+++ include/program_analysis/data_flow_object.hh
@@ -1,42 +1,43 @@
 #ifndef DISTILLERY_DATA_FLOW_OBJECT_HH
 #define DISTILLERY_DATA_FLOW_OBJECT_HH
 
-#include <unordered_map>
-#include <memory>
 #include <graph_abstractions/cfg_interface.hh>
+#include <memory>
+#include <unordered_map>
 
 namespace distillery {
-  template <
-    typename GraphType,
-    typename Value
-    >
-  struct DataFlowObject {
+template<
+  typename GraphType,
+  typename Value>
+struct DataFlowObject
+{
 
-    using Cfg = typename GraphType::Cfg;
-    using Block = typename GraphType::Block;
-    using Statement = typename GraphType::Statement;
+  using Cfg = typename GraphType::Cfg;
+  using Block = typename GraphType::Block;
+  using Statement = typename GraphType::Statement;
 
-    const Cfg &cfg;
+  const Cfg &cfg;
 
-    std::unordered_map<const Block*, std::unique_ptr<Value>> block_in, block_out;
-    std::unordered_map<const Statement*, std::unique_ptr<Value>> statement_in, statement_out;
+  std::unordered_map<const Block *, std::unique_ptr<Value>> block_in, block_out;
+  std::unordered_map<const Statement *, std::unique_ptr<Value>> statement_in, statement_out;
 
-    explicit DataFlowObject(const Cfg &cfg1):cfg(cfg1) {
+  explicit DataFlowObject(const Cfg &cfg1) : cfg(cfg1)
+  {
+  }
+
+  void dump(FILE *out = stdout)
+  {
+    for (auto &block : cfg) {
+      fprintf(out, "Block : %d\nBlock In : \n", block->block_id());
+      block_in[block.get()]->dump(out);
+      fprintf(out, "\n");
+      block->dump(out);
+      fprintf(out, "Block Out : \n");
+      block_out[block.get()]->dump(out);
+      fprintf(out, "\n");
     }
+  }
+};
+}// namespace distillery
 
-    void dump(FILE* out = stdout)  {
-      for(auto &block : cfg) {
-        fprintf(out, "Block : %d\nBlock In : \n", block->block_id());
-        block_in[block.get()]->dump(out);
-        fprintf(out, "\n");
-        block->dump(out);
-        fprintf(out, "Block Out : \n");
-        block_out[block.get()]->dump(out);
-        fprintf(out, "\n");
-      }
-    }
-
-  };
-}
-
-#endif //DISTILLERY_DATA_FLOW_OBJECT_HH
+#endif//DISTILLERY_DATA_FLOW_OBJECT_HH
--- include/program_analysis/program_analysis.hh
+++ include/program_analysis/program_analysis.hh
@@ -1,43 +1,43 @@
 #ifndef DISTILLERY_PROGRAM_ANALYSIS_HH
 #define DISTILLERY_PROGRAM_ANALYSIS_HH
 
-#include <memory>
 #include <map>
+#include <memory>
 
-#include <program_analysis/data_flow_object.hh>
 #include <graph_abstractions/cfg_interface.hh>
+#include <program_analysis/data_flow_object.hh>
 
 
 namespace distillery {
-  template <
-    typename GraphType,
-    typename Value
-    >
-  class ProgramAnalysis {
+template<
+  typename GraphType,
+  typename Value>
+class ProgramAnalysis
+{
 
-    using Cfg = typename GraphType::Cfg;
-    using Block = typename GraphType::Block;
-    using Statement = typename GraphType::Statement;
+  using Cfg = typename GraphType::Cfg;
+  using Block = typename GraphType::Block;
+  using Statement = typename GraphType::Statement;
 
-  public:
-    virtual std::unique_ptr<Value> copy(const Value &value) = 0;
+public:
+  virtual std::unique_ptr<Value> copy(const Value &value) = 0;
 
-    virtual std::unique_ptr<Value> meet(const Value &value_1, const Value &value_2) = 0;
+  virtual std::unique_ptr<Value> meet(const Value &value_1, const Value &value_2) = 0;
 
-    virtual std::unique_ptr<Value> boundary_value(const Cfg &cfg) = 0;
+  virtual std::unique_ptr<Value> boundary_value(const Cfg &cfg) = 0;
 
-    virtual std::unique_ptr<Value> topValue() = 0;
+  virtual std::unique_ptr<Value> topValue() = 0;
 
-    virtual std::unique_ptr<DataFlowObject<GraphType, Value>> doAnalysis(const Cfg &cfg) = 0;
+  virtual std::unique_ptr<DataFlowObject<GraphType, Value>> doAnalysis(const Cfg &cfg) = 0;
 
-    virtual std::unique_ptr<DataFlowObject<GraphType, Value>> doAnalysisFromValue(const Cfg &cfg, const Value &value) = 0;
+  virtual std::unique_ptr<DataFlowObject<GraphType, Value>> doAnalysisFromValue(const Cfg &cfg, const Value &value) = 0;
 
-    virtual std::unique_ptr<Value> post(const Block &block, const Value &value) = 0;
+  virtual std::unique_ptr<Value> post(const Block &block, const Value &value) = 0;
 
-    virtual std::unique_ptr<Value> post_statement(const Statement &statement, const Value &value) = 0;
+  virtual std::unique_ptr<Value> post_statement(const Statement &statement, const Value &value) = 0;
 
-    virtual bool is_equal(const Value &value_1, const Value &value_2) = 0;
-  };
-}
+  virtual bool is_equal(const Value &value_1, const Value &value_2) = 0;
+};
+}// namespace distillery
 
-#endif //DISTILLERY_PROGRAM_ANALYSIS_HH
+#endif//DISTILLERY_PROGRAM_ANALYSIS_HH
