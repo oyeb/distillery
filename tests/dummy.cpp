6ef2ed288576a8dc230c53805e628ff519787c2b
Ananya Bahadur <ananya95@gmail.com>
2020-06-12 02:34:12 +0530

    fix: Segfaults due to bad refs to vector elements in DummyGraph
    The DUmmyGraph constructor builds the blocks vector from size 0 to 5. In
    some compilers, this may kick the resizing login which may invalidate
    some push_successor refs (that's what was happening).
    
    Either the vector shouldn't resize (possible only for default-insertible
    elems -- not DummyBlock)
    Or the refs should be used when the size is fixed.
    
    Partially helps with #1

--- tests/dummy.cpp
+++ tests/dummy.cpp
@@ -1,6 +1,8 @@
 #include "dummy.hh"
 #include "doctest.h"
 
+#include <iostream>
+
 // Tests that don't naturally fit in the headers/.cpp files directly
 // can be placed in a tests/*.cpp file. Integration tests are a good example.
 
@@ -26,20 +28,29 @@ DummyGraph::DummyGraph()
    *            2 --------> 4
    *                  3 --> 4
    */
-  for (int idx = 0; idx < 5; idx++) {
+  for (int idx = 0; idx < 5; idx++)
     blocks.emplace_back(idx);
-    if (idx > 0)
-      blocks[idx - 1].push_successor(&blocks[idx]);
+
+  for (auto &b : blocks) {
+    if (b.id < 4)
+      b.push_successor(&blocks[b.id + 1]);
   }
   blocks[1].push_successor(&blocks[1]);
   blocks[2].push_successor(&blocks[4]);
   root = &blocks[0];
 }
 
-
 TEST_CASE("dummy graph works")
 {
   DummyGraph dg;
+  for (const auto &b : dg.blocks) {
+    bool has_someone_larger = false, has_someone = false;
+    for (const auto &successor : b.successors) {
+      has_someone = true;
+      has_someone_larger |= successor->id > b.id;
+    }
+    CHECK((!has_someone || has_someone_larger));
+  }
   CHECK(dg.doSomething() == true);
   CHECK(dg.blocks.size() == 5);
 }
